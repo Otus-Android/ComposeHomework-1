69ae7d44f804c977b9814e171a133f0c0e1735b7
500a5 <500a5@mail.ru>
2025-04-07 17:01:45 +0300

    Вопросы 1. Как мокать viewModel в @Preview? 2. Можно ли установить shape фигуру как картинку? 3. Если у нас одна активти все viewModel к ней подключаем ? 4. SwipeToRefresh delay?

--- app/src/main/java/ru/otus/marketsample/details/feature/DetailsScreen.kt
+++ app/src/main/java/ru/otus/marketsample/details/feature/DetailsScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
