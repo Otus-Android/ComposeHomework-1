1705e9922ca469f1ed1d3171b2f2578d3ca4e2fc
Bogdan Patseev <patseev1987@gmail.com>
2025-07-07 20:52:43 +0300

    add compose dependencies

--- app/src/main/java/ru/otus/marketsample/MainActivity.kt
+++ app/src/main/java/ru/otus/marketsample/MainActivity.kt
@@ -1,16 +1,28 @@
 package ru.otus.marketsample
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import ru.otus.marketsample.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
 
-class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : ComponentActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Text(text = "Hello World")
+                }
+            }
+        }
     }
 }
